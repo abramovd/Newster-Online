12972ff492159f2dac17d35e835ff15a94be7ee3
Dmitry Abramov <diabramo@ya.ru>
2015-11-01 12:54:52 +0200

    very very simple interface

--- app/views.py
+++ app/views.py
@@ -1,6 +1,36 @@
 from app import app
+from flask import render_template, g, redirect, url_for
+from forms import SearchForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    form = SearchForm()
+    if form.validate_on_submit():
+        redirect(url_for('search_results'), form = form)
+    return render_template("index.html",
+                           title= 'Home', form = form)
+                           
+                           
+                           
+@app.before_request
+def before_request():
+    g.search_form = SearchForm()
+
+@app.route('/search', methods=['POST'])
+def search():
+    if not g.search_form.validate_on_submit():
+        return redirect(url_for('index'))
+    return redirect(url_for('search_results', query=g.search_form.search.data))
+
+
+@app.route('/search_results/<query>')          
+def search_results(query):
+    from newster.base import Newster
+    from newster.config import api_urls, api_keys
+    newster = Newster(api_urls, api_keys, query)
+    snippets = newster.get_snippets()
+    return render_template("search_results.html",
+                           title='Search Results',
+                           query = query,
+                           snippets = snippets)  
