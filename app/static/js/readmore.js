1c65fde06f9f39a6ad318ab1e8bdf26db3cece95
Dmitry Abramov <diabramo@ya.ru>
2015-11-02 16:07:51 +0200

    working prototype

---
+++ app/static/js/readmore.js
@@ -0,0 +1,7 @@
+// Hide the extra content initially, using JS so that if JS is disabled, no problemo.
+$('.read-more-content').addClass('hide');
+
+// Set up the toggle.
+$('.read-more-toggle').on('click', function() {
+  $(this).next('.read-more-content').toggleClass('hide');
+});
--- app/views.py
+++ app/views.py
@@ -5,13 +5,10 @@ from forms import SearchForm
 @app.route('/')
 @app.route('/index')
 def index():
-    form = SearchForm()
-    if form.validate_on_submit():
-        redirect(url_for('search_results'), form = form)
+    if g.search_form.validate_on_submit():
+        redirect(url_for('search'))
     return render_template("index.html",
-                           title= 'Home', form = form)
-                           
-                           
+                           title= 'Home')
                            
 @app.before_request
 def before_request():
@@ -28,16 +25,22 @@ def search():
 def search_results(query):
     from tools.newster.base import Newster
     from tools.newster.config import api_urls, api_keys
+    titles = []
+    links = []
+    snippets = []
+    titles = []
+    sources = []
+    clusters = {}
+    n_clusters = 7
     try:
         newster = Newster(api_urls, api_keys, query)
         snippets = newster.get_snippets()
         titles = newster.get_titles()
         links = newster.get_links()
         sources = newster.get_sources()
-        clusters = newster.find_clusters(method = 'kmeans', n_clusters = 10)
+        clusters = newster.find_clusters(method = 'fca', n_clusters = n_clusters)
     except:
-        titles = []
-        snippets = []
+        pass
     return render_template("search_results.html",
                            title='Search Results',
                            query = query,
@@ -45,4 +48,5 @@ def search_results(query):
                            titles = titles,
                            clusters = clusters,
                            links = links,
-                           sources = sources)  
+                           sources = sources,
+                           n_clusters = n_clusters)  
